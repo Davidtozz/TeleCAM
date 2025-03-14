8c340b2a85096d3f48fbe0d2196068d0e5186ee1
Davide Pulvirenti <davidepulvirenti2013@gmail.com>
2025-03-14 21:49:57 +0100

    [feature] Add static adapter for Svelte and introduced UserInfo component

---
+++ Client/src/lib/components/UserInfo.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { Contact } from "$lib/state/chat.svelte";
+  import { X, EllipsisVertical, Phone } from "@lucide/svelte";
+
+    let {selectedContact}: {selectedContact: Contact | null} = $props();
+</script>
+
+
+<div>
+    <!-- hero section -->
+    <section>
+        <div class="flex items-center justify-between">
+            <p>User Info</p>
+            <div>
+                <Phone size={20} />
+                <EllipsisVertical size={20} />
+                <X size={20} />
+            </div>
+        </div>
+        <div></div>
+    </section>
+    <!-- contact info -->
+    <section></section>
+    <!-- media summary -->
+    <section></section>
+    <!-- actions -->
+    <section></section>
+</div>
